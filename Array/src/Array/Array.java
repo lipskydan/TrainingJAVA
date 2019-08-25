bc1f57b0a2646704b889f935918769ca28584b23
lipskydan <43038176+lipskydan@users.noreply.github.com>
2019-08-25 12:53:59 +0300

    Add 2 Classes
    1 - List
    2 - Menu

--- Array/src/Array/Array.java
+++ Array/src/Array/Array.java
@@ -3,24 +3,56 @@ package Array;
 import java.util.*;
 
 
+
 public class Array {
 
 	public static void main(String[] args) {
-		System.out.println("¬ведите число: ");
-		
+//		System.out.println("¬ведите число: ");
+//		
 		Scanner in = new Scanner(System.in);
-		int size = in.nextInt();
-		int[] a = new int[size];
 		
-		for (int index=0, el=1; index < a.length; index++, el++) {
-			a[index] = el;
-		}
+		Menu m = new Menu();
+		m.menu();
 		
-		for(int el: a) {
-			if (el%2 != 0) System.out.print(" ");
-			System.out.println(el);
-		}
-	
+//		List list = new List();
+//	
+//		list.Add(1);
+//		list.Add(3);
+//		list.Add(2);
+//		list.Add(5);
+//		list.Add(4);
+//		list.Show();
+//		
+//		list.ShowSorted();
+//		
+//		list.Delete();
+//		list.Show();
+		
+		
+//		int[] arr = new int[1];
+//		arr[0] = 7;
+//		
+//		System.out.println(arr[0]);
+//		
+//		arr.length;
+//		arr[1] = 8;
+//		
+//		System.out.println(arr[1]);
+		
+		
+		//list.Show();
+//		int size = in.nextInt();
+//		int[] a = new int[size];
+//		
+//		for (int index=0, el=1; index < a.length; index++, el++) {
+//			a[index] = el;
+//		}
+//		
+//		for(int el: a) {
+//			if (el%2 != 0) System.out.print(" ");
+//			System.out.println(el);
+//		}
+//	
 	}
 
 }
